3b93a0b70f28195541ef6d58a9e44f3bebaa065b
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-22 13:45:19 +0000

    Update wallet.js

--- route/wallet.js
+++ route/wallet.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.json({ msg: 'Wallet route OK' });
+});
+
+module.exports = router;
