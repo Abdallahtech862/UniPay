4f8f1fc87e5f6226717bfff3eec6be3dd1e14b3b
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-22 13:34:19 +0000

    Add server listening configuration

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Serveur sur port ${PORT}`));
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
