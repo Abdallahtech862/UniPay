58337526a3454a197b44158d6eb189ec0038c67e
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-23 19:05:32 +0000

    Add cancellation fields to Transaction schema

--- models/Transaction.js
+++ models/Transaction.js
@@ -20,6 +20,35 @@ const transactionSchema = new mongoose.Schema({
   date: {
     type: Date,
     default: Date.now
+  },
+  annulee: {
+    type: Boolean,
+    default: false
+  },
+  dateAnnulation: Date
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
+const transactionSchema = new mongoose.Schema({
+  expediteur: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true
+  },
+  destinataire: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true
+  },
+  montant: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  motif: String,
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
 
