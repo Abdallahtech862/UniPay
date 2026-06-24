417fd009ea93d0afcc8ffc59cd200170a1d14973
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-24 11:50:38 +0000

    Update Client.js

--- models/Client.js
+++ models/Client.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose');
 const clientSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   prenom: { type: String, required: true },
+  pseudo: { type: String, required: true, unique: true },
   telephone: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   solde: { type: Number, default: 0 },
   password: { type: String, required: true },
   role: { type: String, default: 'client' },
-  
-  // Nouveaux champs
-  limiteJournaliere: { type: Number, default: 500000 }, // 500k FCFA par défaut
-  limiteMensuelle: { type: Number, default: 5000000 },   // 5M FCFA par défaut
-  
+
+  limiteJournaliere: { type: Number, default: 500000 },
+  limiteMensuelle: { type: Number, default: 5000000 },
+
+  photoProfil: String, // URL Cloudinary ou chemin
+  carteRecto: String,
+  carteVerso: String,
+
   dateCreation: { type: Date, default: Date.now }
 });
 
