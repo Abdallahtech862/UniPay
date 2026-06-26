278a6795ced6bebb69a4efbd17ba39cbc567be18
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-26 10:30:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 //app.use(express.json());
