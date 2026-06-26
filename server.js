dae045ce9d2376ad78827dac82d1a6cdb5afd79b
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-26 10:38:12 +0000

    Enable CORS middleware in server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const cors = require('cors');
-app.use(cors({
-  origin: '*', // Accepte tout pour tester
-  credentials: true
+//const cors = require('cors');
+//app.use(cors({
+//  origin: '*', // Accepte tout pour tester
+//  credentials: true
 }));
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 //app.use(express.json());
