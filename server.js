e92e2376e92f21d5a5083b77db7ed32c93f719e6
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-26 10:06:28 +0000

    Set body size limit to 10mb for JSON and URL-encoded data
    Increase the JSON and URL-encoded body size limit to 10mb.

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(cors({
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Pour les formulaires HTML
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true })); // Pour les formulaires HTML
 
 process.on('uncaughtException', (err) => {
   console.error('FATAL:', err.message);
