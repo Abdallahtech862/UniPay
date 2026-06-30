9f6331fed9bc1059eb36ac988e558b92024ff387
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-30 20:50:05 +0000

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,24 +1,16 @@
 const mongoose = require('mongoose');
 
-const transactionSchemaa = new mongoose.Schema({
-  expediteur: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
-  destinataire: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
-  montant: { type: Number, required: true, min: 1 },
-  frais: { type: Number, default: 0 },
-  motif: String,
-  status: { type: String, enum: ['validee', 'annulee', 'en_attente'], default: 'validee' },
-  soldeExpediteurApres: Number, // ✅ nouveau
-  soldeDestinataireApres: Number, // ✅ nouveau
-  annulee: { type: Boolean, default: false },
-  dateAnnulation: Date
-}, { timestamps: true });
-//module.exports = mongoose.model('Transaction', transactionSchema);
-
-//const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
   expediteur: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
-  destinataire: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' }, // ✅ Plus required
+  //destinataire: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' }, // ✅ Plus required
+  destinataire: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Client',
+  required: function() {
+    return this.type === 'envoi' || this.type === 'reception';
+  }
+},
   type: {
     type: String,
     enum: ['envoi', 'reception', 'retrait', 'recharge'],
