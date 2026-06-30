64d58643e84621f90168c0f52144f1a5276fdd31
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-06-30 13:55:17 +0000

    Refactor transaction schema with new fields
    Updated transaction schema to include new fields and modify existing ones.

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
+const transactionSchemaa = new mongoose.Schema({
   expediteur: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
   destinataire: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
   montant: { type: Number, required: true, min: 1 },
@@ -12,4 +12,30 @@ const transactionSchema = new mongoose.Schema({
   annulee: { type: Boolean, default: false },
   dateAnnulation: Date
 }, { timestamps: true });
+//module.exports = mongoose.model('Transaction', transactionSchema);
+
+//const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  expediteur: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
+  destinataire: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' }, // ✅ Plus required
+  type: {
+    type: String,
+    enum: ['envoi', 'reception', 'retrait', 'recharge'],
+    required: true
+  },
+  montant: { type: Number, required: true, min: 1 },
+  frais: { type: Number, default: 0 },
+  motif: String,
+  status: { type: String, enum: ['validee', 'annulee', 'en_attente'], default: 'en_attente' },
+  soldeExpediteurApres: Number,
+  soldeDestinataireApres: Number,
+  // Champs spécifiques retrait
+  operateur: String, // MTN Money, Wave, etc
+  numeroDestination: String, // numéro mobile money ou carte
+  annulee: { type: Boolean, default: false },
+  dateAnnulation: Date,
+  dateValidation: Date
+}, { timestamps: true });
+
 module.exports = mongoose.model('Transaction', transactionSchema);
