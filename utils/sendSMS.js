461d6e9022ecc389d4e95457d012e166d3b40efe
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-07-05 10:08:19 +0000

    Update sendSMS.js

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,54 +1,23 @@
-const axios = require('axios');
-
-async function getOrangeToken() {
-  const auth = Buffer.from(`${process.env.ORANGE_CLIENT_ID}:${process.env.ORANGE_CLIENT_SECRET}`).toString('base64');
-  
-  const res = await axios.post(
-    'https://api.orange.com/oauth/v3/token',
-    'grant_type=client_credentials',
-    {
-      headers: {
-        'Authorization': `Basic ${auth}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
+const response = await axios.post(
+  `https://api.orange.com/smsmessaging/v1/outbound/${process.env.ORANGE_SENDER_ADDRESS}/requests`,
+  {
+    outboundSMSMessageRequest: {
+      address: formattedNumber,
+      senderAddress: process.env.ORANGE_SENDER_ADDRESS,
+      senderName: process.env.ORANGE_SENDER_NAME,
+      outboundSMSTextMessage: {
+        message
       }
     }
-  );
-  return res.data.access_token;
-}
-
-async function sendSMSOrange(phoneNumber, message) {
-  console.log('ok');
-  try {
-    const token = await getOrangeToken();
-    //console.error('token:', token);
-    // Orange veut tel:+226XXXXXXXX
-    const formattedNumber = phoneNumber.startsWith('+226') 
-      ? `tel:${phoneNumber}` 
-      : `tel:+226${phoneNumber.slice(-8)}`;
-      console.error('SMS Orange error:', token);
-    await axios.post(
-      `https://api.orange.com/smsmessaging/v1/outbound/${process.env.ORANGE_SENDER_ADDRESS}/requests`,
-      {
-        outboundSMSMessageRequest: {
-          address: formattedNumber,
-          senderAddress: process.env.ORANGE_SENDER_ADDRESS,
-          senderName: process.env.ORANGE_SENDER_NAME,
-          outboundSMSTextMessage: { message }
-        }
-      },
-      {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-    console.error('requests:',requests);
-    return true;
-  } catch (err) {
-    console.error('SMS Orange error:', err.response?.data || err.message);
-    return false;
+  },
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
   }
-}
+);
 
-module.exports = { sendSMSOrange };
+console.log("Réponse Orange :", response.data);
+
+return true;
