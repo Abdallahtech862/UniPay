d7ca9e59258ff386c8b533d645930bcbe6651243
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-07-06 07:29:27 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -209,7 +209,12 @@ router.post('/register', upload.fields([
     await client.save(); // Plus de pre('save') donc pas de double hash
     
     const token = jwt.sign({ id: client._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    res.status(201).json({ message: 'Compte créé', token, user});
+    //res.status(201).json({ message: 'Compte créé', token, user});
+    res.status(201).json({
+  message: 'Compte créé',
+  token,
+  user: client
+});
     
   } catch (err) {
     console.error('Erreur register:', err);
