fd538d06696ab2382f61c1829a5be48bd837b02d
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-07-12 18:04:41 +0000

    Fix whitespace in terms HTML response
    Remove unnecessary whitespace in the terms HTML response.

--- routes/legal.js
+++ routes/legal.js
@@ -86,7 +86,7 @@ router.get('/privacy', (req, res) => {
 router.get('/terms', (req, res) => {
   res.send(`
     <!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conditions d'utilisation - UniPay</title>${baseStyle}</head>
     <body>
       <div class="container">
