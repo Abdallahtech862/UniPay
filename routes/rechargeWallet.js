ad120f60f9dabc69dd7421dfd281274c0ae0022c
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-07-14 17:11:43 +0000

    Update rechargeWallet.js

--- routes/rechargeWallet.js
+++ routes/rechargeWallet.js
@@ -7,8 +7,8 @@ const Client = require('../models/Client');
 const Transaction = require('../models/Transaction');
 const { authUser } = require('../middleware/auth');
 
-const PAWAPAY_API_KEY = process.env.PAWAPAY_API_KEY;
-const PAWAPAY_BASE_URL = process.env.PAWAPAY_BASE_URL || 'https://api.sandbox.pawapay.io';
+const PAWAPAY_API_KEY=process.env.PAWAPAY_API_KEY;
+const PAWAPAY_BASE_URL=process.env.PAWAPAY_BASE_URL || 'https://api.sandbox.pawapay.io';
 
 const PROVIDER_CONFIG = {
   '221': { currency: 'XOF', operators: { orange: 'ORANGE_SEN', free: 'FREE_SEN' } },
