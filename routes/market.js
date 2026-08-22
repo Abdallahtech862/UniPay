f5d6d01a92ad647b1c5127ec50ba6b940f92476a
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-08-22 20:39:10 +0000

    Update market.js

--- routes/market.js
+++ routes/market.js
@@ -77,7 +77,7 @@ router.post('/orders/pay',verifyToken, async (req, res) => {
     if (produit.statut!== 'actif') return res.status(400).json({ erreur: 'Produit non disponible' });
 
     const acheteur = await Utilisateur.findById(req.acheteur);
-    onsole.log('vendeurId reçu:', vendeurId);
+    console.log('vendeurId reçu:', vendeurId);
     if (acheteur.solde < produit.prix) return res.status(400).json({ erreur: 'Solde insuffisant' });
 
     acheteur.solde -= produit.prix;
