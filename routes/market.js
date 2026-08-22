e59aae081209605516418cf6726a16913cb5c2b8
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-08-22 18:20:59 +0000

    Change console log from userId to vendeurId

--- routes/market.js
+++ routes/market.js
@@ -10,7 +10,7 @@ const Utilisateur = require('../models/Client');
 const { verifyAdmin, authUser, verifyToken } = require('../middleware/auth');
 // 1. Créer un produit
 router.post('/products', async (req, res) => {
-  console.log('error:',req.userId);
+  console.log('error:',req.vendeurId);
   try {
     const vendeur = await Utilisateur.findById(req.userId);
     const produit = await Produit.create({
