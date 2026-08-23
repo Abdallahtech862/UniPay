257f5a9917d17560efa0f677df36cfb8a24cf1ce
Sawadogo Abdoulaye <sawabdoulaye7542@gmail.com>
2026-08-23 13:42:42 +0000

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ module.exports = { Message, Produit, Commande };
 
 // ================== ROUTES API ==================
 app.use('/api/legal', require('./routes/legal'));
-app.use('/product', require('./routes/product'));
+app.use('/product', require('./routes/deeplink'));
 app.use('/api/transactions', require('./routes/transactions'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/wallet', require('./routes/wallet'));
